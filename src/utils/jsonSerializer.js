cc6d77d21192f8b0eae0ec342d695034cedb34c0
Larissa2801 <110750076+Larissa2801@users.noreply.github.com>
2025-11-14 22:13:24 -0300

    chore: Remove pasta 'src' duplicada na raiz.

--- src/utils/jsonSerializer.js
+++
@@ -1,8 +0,0 @@
-// Adiciona a função toJSON ao protótipo BigInt
-// Isso permite que JSON.stringify() converta BigInts para strings.
-BigInt.prototype.toJSON = function () {
-  return this.toString();
-};
-
-// Opcional: exporta uma função dummy ou nada, se você apenas quiser que seja executado.
-module.exports = true;
